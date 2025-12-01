796f112d16bf1592b99835d370e2038d1468875b
raksa <eng.raksa@gmail.com>
2025-11-30 22:39:08 -0500

    fix(bibleModelHelpers): add support for KJVD Bible model and update related enums

--- src/helper/bible-helpers/bibleModelHelpers.ts
+++ src/helper/bible-helpers/bibleModelHelpers.ts
@@ -1,7 +1,7 @@
 import { freezeObject } from '../helpers';
 
 import kjvBibleJson from './kjvBible.json';
-import kjv1611BibleJson from './kjvBible1611.json';
+import kjvdBibleJson from './kjvdBible.json';
 import douayRheimsBibleJson from './douayRheimsBible.json';
 
 import kjvNewLiners from './kjvNewLiners.json';
@@ -22,32 +22,32 @@ export type BibleModelInfoType = {
     oneChapterBooks: string[];
 };
 
-export enum BibleModelInfoEnum {
-    KJV = 'KJV',
-    KJV1611 = 'KJV1611',
-    DR = 'DR',
-}
-export const bibleModelInfoTitleMap: { [key in BibleModelInfoEnum]: string } = {
-    [BibleModelInfoEnum.KJV]: 'King James Version',
-    [BibleModelInfoEnum.KJV1611]: 'King James Version 1611',
-    [BibleModelInfoEnum.DR]: 'Douay Rheims',
-};
-
 const kjvBibleModelInfo = kjvBibleJson as BibleModelInfoType;
 freezeObject(kjvBibleModelInfo);
-const kjv1611BibleModelInfo = kjv1611BibleJson as BibleModelInfoType;
-freezeObject(kjv1611BibleModelInfo);
+const kjvdBibleModelInfo = kjvdBibleJson as BibleModelInfoType;
+freezeObject(kjvdBibleModelInfo);
 const douayRheimsBibleModelInfo = douayRheimsBibleJson as BibleModelInfoType;
 freezeObject(douayRheimsBibleModelInfo);
 
 export const kjvNewLinerInfo = kjvNewLiners;
 freezeObject(kjvNewLinerInfo);
 
+export enum BibleModelInfoEnum {
+    KJV = 'KJV',
+    KJVD = 'KJVD',
+    DR = 'DR',
+}
+export const bibleModelInfoTitleMap: { [key in BibleModelInfoEnum]: string } = {
+    [BibleModelInfoEnum.KJV]: kjvBibleModelInfo.title,
+    [BibleModelInfoEnum.KJVD]: kjvdBibleModelInfo.title,
+    [BibleModelInfoEnum.DR]: douayRheimsBibleModelInfo.title,
+};
+
 const MODEL_BIBLE_INFO_SETTING_NAME = 'model-bible-info';
 export function getBibleModelInfoSetting() {
     const setting = getSetting(MODEL_BIBLE_INFO_SETTING_NAME);
     if (
-        setting === BibleModelInfoEnum.KJV1611 ||
+        setting === BibleModelInfoEnum.KJVD ||
         setting === BibleModelInfoEnum.DR
     ) {
         return setting;
@@ -60,8 +60,8 @@ export function setBibleModelInfoSetting(model: BibleModelInfoEnum) {
 
 export function getBibleModelInfo() {
     const setting = getBibleModelInfoSetting();
-    if (setting === BibleModelInfoEnum.KJV1611) {
-        return kjv1611BibleModelInfo;
+    if (setting === BibleModelInfoEnum.KJVD) {
+        return kjvdBibleModelInfo;
     }
     if (setting === BibleModelInfoEnum.DR) {
         return douayRheimsBibleModelInfo;
