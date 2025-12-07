3fa170730f43abb0dd8306390a4d15dc3c79fc76
raksa <eng.raksa@gmail.com>
2025-12-07 17:54:18 -0500

    feat: implement AI settings handling with user input for OpenAI and Anthropic API keys

--- src/bible-reader/AIConfigComp.tsx
+++ src/bible-reader/AIConfigComp.tsx
@@ -6,6 +6,113 @@ import {
 import { showAppInput } from '../popup-widget/popupWidgetHelpers';
 import appProvider from '../server/appProvider';
 
+async function handleAISetting() {
+    if (!appProvider.isPageReader) {
+        return;
+    }
+    const openAISetting = getAISetting();
+    let openAIAPIKey = openAISetting.openAIAPIKey;
+    let anthropicAPIKey = openAISetting.anthropicAPIKey;
+    const isConfirmInput = await showAppInput(
+        'Audio AI Setting',
+        <>
+            <div className="d-flex flex-column w-100 h-100">
+                <div>
+                    OpenAI API Key{' '}
+                    <i
+                        className="bi bi-lightbulb"
+                        title="`This key will be used in custom Bible Cross Ref and Bible Audio"
+                        style={{
+                            color: 'var(--bs-info-text-emphasis)',
+                        }}
+                    />
+                    :
+                </div>
+                <div className="flex-grow-1 d-flex align-items-center">
+                    <input
+                        ref={(input) => {
+                            if (input) {
+                                input.value = openAIAPIKey;
+                                setTimeout(() => {
+                                    input.focus();
+                                    input.select();
+                                }, 100);
+                            }
+                        }}
+                        className="form-control form-control-sm flex-fill mx-2"
+                        type="text"
+                        onChange={(e) => {
+                            openAIAPIKey = e.target.value;
+                        }}
+                    />
+                    <button
+                        className="btn btn-sm btn-secondary"
+                        title="`Create OpenAI api key"
+                        onClick={async () => {
+                            appProvider.browserUtils.openExternalURL(
+                                'https://platform.openai.com/api-keys',
+                            );
+                        }}
+                    >
+                        API Key <i className="bi bi-box-arrow-up-right ms-1" />
+                    </button>
+                </div>
+            </div>
+            <hr />
+            <div className="d-flex flex-column w-100 h-100 mb-2">
+                <div>
+                    Anthropic API Key{' '}
+                    <i
+                        className="bi bi-lightbulb"
+                        title="`This key will be used in custom Bible Cross Ref"
+                        style={{
+                            color: 'var(--bs-info-text-emphasis)',
+                        }}
+                    />
+                    :
+                </div>
+                <div className="flex-grow-1 d-flex align-items-center">
+                    <input
+                        ref={(input) => {
+                            if (input) {
+                                input.value = anthropicAPIKey;
+                                setTimeout(() => {
+                                    input.focus();
+                                    input.select();
+                                }, 100);
+                            }
+                        }}
+                        className="form-control form-control-sm flex-fill mx-2"
+                        type="text"
+                        onChange={(e) => {
+                            anthropicAPIKey = e.target.value;
+                        }}
+                    />
+                    <button
+                        className="btn btn-sm btn-secondary"
+                        title="`Create Anthropic api key"
+                        onClick={async () => {
+                            appProvider.browserUtils.openExternalURL(
+                                'https://console.anthropic.com/settings/keys',
+                            );
+                        }}
+                    >
+                        API Key <i className="bi bi-box-arrow-up-right ms-1" />
+                    </button>
+                </div>
+            </div>
+        </>,
+    );
+    if (!isConfirmInput) {
+        return;
+    }
+    setAISetting({
+        ...openAISetting,
+        openAIAPIKey,
+        anthropicAPIKey,
+    });
+}
+
 function AISettingComp() {
     const aiSetting = useAISetting();
     return (
@@ -17,111 +124,7 @@ function AISettingComp() {
                     color: aiSetting.openAIAPIKey ? 'green' : '',
                     fontSize: '25px',
                 }}
-                onClick={async () => {
-                    const openAISetting = getAISetting();
-                    let openAIAPIKey = openAISetting.openAIAPIKey;
-                    let anthropicAPIKey = openAISetting.anthropicAPIKey;
-                    const isConfirmInput = await showAppInput(
-                        'Audio AI Setting',
-                        <>
-                            <div className="d-flex flex-column w-100 h-100">
-                                <div>
-                                    OpenAI API Key{' '}
-                                    <i
-                                        className="bi bi-lightbulb"
-                                        title="`This key will be used in custom Bible Cross Ref and Bible Audio"
-                                        style={{
-                                            color: 'var(--bs-info-text-emphasis)',
-                                        }}
-                                    />
-                                    :
-                                </div>
-                                <div className="flex-grow-1 d-flex align-items-center">
-                                    <input
-                                        ref={(input) => {
-                                            if (input) {
-                                                input.value = openAIAPIKey;
-                                                setTimeout(() => {
-                                                    input.focus();
-                                                    input.select();
-                                                }, 100);
-                                            }
-                                        }}
-                                        className="form-control form-control-sm flex-fill mx-2"
-                                        type="text"
-                                        onChange={(e) => {
-                                            openAIAPIKey = e.target.value;
-                                        }}
-                                    />
-                                    <button
-                                        className="btn btn-sm btn-secondary"
-                                        title="`Create OpenAI api key"
-                                        onClick={async () => {
-                                            appProvider.browserUtils.openExternalURL(
-                                                'https://platform.openai.com/api-keys',
-                                            );
-                                        }}
-                                    >
-                                        API Key
-                                        <i className="bi bi-box-arrow-up-right ms-1" />
-                                    </button>
-                                </div>
-                            </div>
-                            <hr />
-                            <div className="d-flex flex-column w-100 h-100 mb-2">
-                                <div>
-                                    Anthropic API Key{' '}
-                                    <i
-                                        className="bi bi-lightbulb"
-                                        title="`This key will be used in custom Bible Cross Ref"
-                                        style={{
-                                            color: 'var(--bs-info-text-emphasis)',
-                                        }}
-                                    />
-                                    :
-                                </div>
-                                <div className="flex-grow-1 d-flex align-items-center">
-                                    <input
-                                        ref={(input) => {
-                                            if (input) {
-                                                input.value = anthropicAPIKey;
-                                                setTimeout(() => {
-                                                    input.focus();
-                                                    input.select();
-                                                }, 100);
-                                            }
-                                        }}
-                                        className="form-control form-control-sm flex-fill mx-2"
-                                        type="text"
-                                        onChange={(e) => {
-                                            anthropicAPIKey = e.target.value;
-                                        }}
-                                    />
-                                    <button
-                                        className="btn btn-sm btn-secondary"
-                                        title="`Create Anthropic api key"
-                                        onClick={async () => {
-                                            appProvider.browserUtils.openExternalURL(
-                                                'https://console.anthropic.com/settings/keys',
-                                            );
-                                        }}
-                                    >
-                                        API Key
-                                        <i className="bi bi-box-arrow-up-right ms-1" />
-                                    </button>
-                                </div>
-                            </div>
-                        </>,
-                    );
-                    if (!isConfirmInput) {
-                        return;
-                    }
-                    setAISetting({
-                        ...openAISetting,
-                        openAIAPIKey,
-                        anthropicAPIKey,
-                    });
-                }}
+                onClick={handleAISetting}
             />
         </div>
     );
