56d0e71198060f10dfaa5421b3fbff6c9152ca4e
raksa <eng.raksa@gmail.com>
2026-03-14 11:46:58 -0400

    feat: add beforeunload event listener to clear search string on page unload

--- src/finder.tsx
+++ src/finder.tsx
@@ -3,6 +3,11 @@ import { StrictMode } from 'react';
 import { init } from './boot';
 import FinderAppComp from './find/FinderAppComp';
 import { getReactRoot } from './others/rootHelpers';
+import { findString } from './find/finderHelpers';
+
+globalThis.addEventListener('beforeunload', () => {
+    findString('');
+});
 
 await init();
 const root = getReactRoot();
