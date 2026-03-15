4991a4f7fe8e65e90e7d2d3f737a5a9afdbd9995
raksa <eng.raksa@gmail.com>
2026-03-15 11:11:15 -0400

    refactor: extract initFontFamily function for better readability and organization

--- src/boot.ts
+++ src/boot.ts
@@ -1,7 +1,7 @@
 import { getCurrentLocale, getLangDataAsync } from './lang/langHelpers';
 import { getAppFontFamily, getAppFontWeight } from './setting/settingHelpers';
 
-export async function init(callback: () => void = () => {}) {
+async function initFontFamily() {
     const id = 'app-custom-style';
     let style = document.getElementById(id) as HTMLStyleElement | null;
     if (style === null) {
@@ -25,6 +25,10 @@ export async function init(callback: () => void = () => {}) {
         }
     `;
     }
+}
+
+export async function init(callback: () => void = () => {}) {
+    initFontFamily();
     const currentLocale = getCurrentLocale();
     await getLangDataAsync(currentLocale);
     callback();
