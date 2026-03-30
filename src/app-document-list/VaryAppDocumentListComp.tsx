53f80e044c81ae14b683c51e4549a6ecdb1f2755
raksa <eng.raksa@gmail.com>
2026-03-29 22:46:04 -0400

    feat: implement context menu for downloading documents from URL with progress feedback

--- src/app-document-list/VaryAppDocumentListComp.tsx
+++ src/app-document-list/VaryAppDocumentListComp.tsx
@@ -4,12 +4,20 @@ import FileListHandlerComp from '../others/FileListHandlerComp';
 import VaryAppDocumentFileComp from './VaryAppDocumentFileComp';
 import AppDocument from './AppDocument';
 import {
+    fsMove,
+    getDownloadPath,
     getFileDotExtension,
     getFileFullName,
     getMimetypeExtensions,
+    mimetypePdf,
+    mimetypePptx,
+    pathJoin,
 } from '../server/fileHelpers';
 import FileSource from '../helper/FileSource';
-import { defaultDataDirNames } from '../helper/constants';
+import {
+    defaultDataDirNames,
+    dirSourceSettingNames,
+} from '../helper/constants';
 import type { DroppedFileType } from '../others/droppingFileHelpers';
 import {
     checkIsPdf,
@@ -17,11 +25,27 @@ import {
     checkIsVaryAppDocumentOnScreen,
     convertOfficeFile,
     supportOfficeFileExtensions,
-    useVaryAppDocumentDirSource,
     varyAppDocumentFromFilePath,
 } from './appDocumentHelpers';
 import type DirSource from '../helper/DirSource';
 import { tran } from '../lang/langHelpers';
+import {
+    type ContextMenuItemType,
+    showAppContextMenu,
+} from '../context-menu/appContextMenuHelpers';
+import {
+    askForURL,
+    messageCallback,
+    streamDownloadFile,
+} from '../background/downloadHelper';
+import { showSimpleToast } from '../toast/toastHelpers';
+import {
+    hideProgressBar,
+    showProgressBar,
+} from '../progress-bar/progressBarHelpers';
+import { handleError } from '../helper/errorHelpers';
+import { initHttpRequest } from '../helper/bible-helpers/downloadHelpers';
+import { useGenDirSourceReload } from '../helper/dirSourceHelpers';
 
 function handleExtraFileChecking(filePath: string) {
     const fileSource = FileSource.getInstance(filePath);
@@ -81,11 +105,95 @@ async function checkIsOnScreen(filePaths: string[]) {
     return false;
 }
 
+async function genContextMenuItems(dirSource: DirSource) {
+    if (dirSource.dirPath === '') {
+        return [];
+    }
+    const contextMenuItems: ContextMenuItemType[] = [];
+    const title = tran('Download From URL');
+    contextMenuItems.push({
+        menuElement: title,
+        onSelect: async () => {
+            const documentUrl = await askForURL(title, 'Documents URL:');
+            if (documentUrl === null) {
+                return;
+            }
+            const downloadDirPath = getDownloadPath();
+            const downloadDestFilePath = pathJoin(
+                downloadDirPath,
+                `${crypto.randomUUID()}.owa-downloading`,
+            );
+            try {
+                showSimpleToast(
+                    title,
+                    `Downloading document from "${documentUrl}", please wait...`,
+                );
+                showProgressBar(documentUrl);
+                messageCallback('Downloading file...');
+                const response = await initHttpRequest(new URL(documentUrl));
+                await streamDownloadFile(
+                    downloadDestFilePath,
+                    response,
+                    messageCallback,
+                );
+                let fileFullName = getFileFullName(documentUrl);
+                if (!fileFullName) {
+                    fileFullName = `downloaded-document-${Date.now()}`;
+                }
+                const destFilePath = pathJoin(dirSource.dirPath, fileFullName);
+                const fileSource = FileSource.getInstance(destFilePath);
+                const nextDestFilePath = await fileSource.genNextFilePath();
+                await fsMove(downloadDestFilePath, nextDestFilePath);
+                showSimpleToast(title, 'Document downloaded successfully');
+            } catch (error) {
+                handleError(error);
+                showSimpleToast(
+                    title,
+                    'Error occurred during downloading document',
+                );
+            } finally {
+                hideProgressBar(documentUrl);
+            }
+        },
+    });
+    return contextMenuItems;
+}
+
+async function handleItemsAdding(
+    dirSource: DirSource,
+    defaultContextMenuItems: ContextMenuItemType[],
+    event: any,
+) {
+    const contextMenuItems = await genContextMenuItems(dirSource);
+    showAppContextMenu(event, [
+        ...defaultContextMenuItems,
+        ...contextMenuItems,
+    ]);
+}
+
 export default function VaryAppDocumentListComp() {
-    const dirSource = useVaryAppDocumentDirSource();
+    const dirSource = useGenDirSourceReload(dirSourceSettingNames.APP_DOCUMENT);
     if (dirSource === null) {
         return null;
     }
+    dirSource.checkExtraFile = (fileFullName: string) => {
+        if (checkIsPdf(getFileDotExtension(fileFullName))) {
+            return {
+                fileFullName: fileFullName,
+                appMimetype: mimetypePdf,
+            };
+        }
+        if (checkIsPptx(getFileDotExtension(fileFullName))) {
+            if (fileFullName.startsWith('~$')) {
+                return null;
+            }
+            return {
+                fileFullName: fileFullName,
+                appMimetype: mimetypePptx,
+            };
+        }
+        return null;
+    };
     const fileSelectionOption = {
         windowTitle: 'Select slide files',
         dirPath: dirSource.dirPath,
@@ -115,6 +223,7 @@ export default function VaryAppDocumentListComp() {
             bodyHandler={handleBodyRendering}
             checkIsOnScreen={checkIsOnScreen}
             fileSelectionOption={fileSelectionOption}
+            onItemsAdding={handleItemsAdding.bind(null, dirSource)}
         />
     );
 }
